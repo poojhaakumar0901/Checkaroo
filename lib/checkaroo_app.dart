6f3443a22f71f64e8c68082125019ef7a25d3e13
poojhaakumar0901 <poojhaakumar0901@gmail.com>
2025-07-10 10:15:56 +0530

    first commit

---
+++ lib/checkaroo_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CheckarooApp extends StatelessWidget {
+  const CheckarooApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+
+import 'package:checkaroo/checkaroo_app.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(CheckarooApp());
+}
